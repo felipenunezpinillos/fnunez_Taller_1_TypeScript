77289f51e895130fa0baff9b2b6cbf0c309967e1
felipenunezpinillos <f.nunez@uniandes.edu.co>
2022-04-21 23:15:14 -0500

    Taller 2 Modificaciones

--- scripts/serie.js
+++ scripts/serie.js
@@ -1,10 +1,13 @@
 var Serie = /** @class */ (function () {
     /* Constructor */
-    function Serie(newNumber, newName, newChannel, newSeasons) {
+    function Serie(newNumber, newName, newChannel, newSeasons, newDescripcion, newLink, newImg) {
         this.number = newNumber;
         this.name = newName;
         this.channel = newChannel;
         this.seasons = newSeasons;
+        this.descripcion = newDescripcion;
+        this.link = newLink;
+        this.img = newImg;
     }
     return Serie;
 }());
--- serie.ts
+++ serie.ts
@@ -5,12 +5,18 @@ export class Serie {
     name: string;
     channel: string;
     seasons: number;
+    descripcion : string;
+    link : string;
+    img : string;
     /* Constructor */
-    constructor(newNumber : number, newName : string, newChannel : string, newSeasons : number)
+    constructor(newNumber : number, newName : string, newChannel : string, newSeasons : number, newDescripcion : string, newLink :string, newImg : string)
     {
         this.number = newNumber;
         this.name = newName;
         this.channel = newChannel;
         this.seasons = newSeasons;
+        this.descripcion = newDescripcion;
+        this.link = newLink;
+        this.img = newImg;
     }
 }
